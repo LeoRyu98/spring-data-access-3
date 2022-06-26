075f56271acfbe913c1b3ac9d0ba887ac7be8b4d
kyeongwoo <drd9811@naver.com>
2022-06-26 22:41:22 +0900

    update 220626

--- src/main/java/hello/springtx/order/OrderService.java
+++ src/main/java/hello/springtx/order/OrderService.java
@@ -21,11 +21,11 @@ public class OrderService {
         log.info("결제 프로세스 진입");
         if (order.getUsername().equals("예외")) {
             log.info("시스템 예외 발생");
-            throw new RuntimeException("시스템 예외"); // rollback
+            throw new RuntimeException("시스템 예외"); // 언체크 예외 : rollback
         } else if(order.getUsername().equals("잔고부족")) {
             log.info("잔고 부족 비지니스 예외 발생");
             order.setPayStatus("대기");
-            throw new NotEnoughMoneyException("잔고가 부족합니다"); // commit
+            throw new NotEnoughMoneyException("잔고가 부족합니다"); // 체크 예외 : commit
         } else {
             // 정상 승인 : commit
             log.info("정상 승인");
